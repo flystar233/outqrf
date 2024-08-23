cbfe1b894b6adbfc660d11fa075cd6ac20bb4fce
Xfly <18374858141@163.com>
2024-08-23 11:44:56 +0800

    Update generateOutliers.r

--- R/generateOutliers.r
+++ R/generateOutliers.r
@@ -20,10 +20,22 @@ generateOutliers <- function(data, p = 0.5, sd_factor = 5, seed = NULL){
     }
     data<- as.data.frame(data)
     numeric_features <- names(data)[sapply(data,is.numeric)]
+
+    max_decimal_places <- function(vec) { #/
+    # Function to find the maximum number of decimal places in a vector
+    #
+    vec_str <- as.character(vec)
+    split_vec <- strsplit(vec_str, "\\.")
+    decimal_places <- sapply(split_vec, function(x) ifelse(length(x) > 1, nchar(x[2]), 0))
+    max_decimal_places <- max(decimal_places)
+    return(max_decimal_places)
+    }
+
     for (features in numeric_features){
         n <- length(data[,features])
         m <- round(p * n)
-        data[sample(n,m),features] <- round(data[m,features] + sd_factor  * sample(c(-1, 1), m, replace = TRUE) * rnorm(m,sd(data[[features]])),2)
+        round_n <- max_decimal_places(data[,features])
+        data[sample(n,m),features] <- round(data[m,features] + sd_factor  * sample(c(-1, 1), m, replace = TRUE) * rnorm(m,sd(data[[features]])),round_n)
     }
     return(data)
 }
