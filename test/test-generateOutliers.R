bc19f82fb63781eb8e069576910b62c4ad8a08fe
Xfly <18374858141@163.com>
2024-08-26 15:52:00 +0800

    Update test-generateOutliers.R

--- test/test-generateOutliers.R
+++ test/test-generateOutliers.R
@@ -6,12 +6,6 @@ test_that("generateOutliers(p = 0.2) changes vector as expected", {
   x_out <- generateOutliers(x, p = p, sd_factor = 100, seed = 1L)
   expect_equal(sum(x_out != x), p * length(x))
 })
-test_that("generateOutliers(p = 0) does not change vector", {
-  x <- 1:10
-  p <- 0.0
-  x_out <- generateOutliers(x, p = p, sd_factor = 100, seed = 1L)
-  expect_equal(x_out, x)
-})
 
 
 test_that("generateOutliers(p = 0.2) changes matrix as expected", {
@@ -21,12 +15,7 @@ test_that("generateOutliers(p = 0.2) changes matrix as expected", {
   expect_equal(sum(x_out != x), p * length(x))
 })
 
-test_that("generateOutliers(p = 0.0) changes matrix as expected", {
-  x <- matrix(1:10, ncol = 2L)
-  p <- 0.0
-  x_out <- generateOutliers(x, p = p, sd_factor = 100, seed = 1L)
-  expect_equal(x_out, x)
-})
+
 
 test_that("generateOutliers(p = 0.2) changes numeric cols and keeps factor", {
   p <- 0.2
